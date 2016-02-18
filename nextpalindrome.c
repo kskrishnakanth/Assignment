554977679db801eaea688ee0d3025ccff552dc1b
kskrishnakanth <kskrishnakanth3@gmail.com>
2016-02-18 20:44:29 +0530

    Update nextpalindrome.c

--- nextpalindrome.c
+++ nextpalindrome.c
@@ -13,15 +13,13 @@ PseudoCode
 
 
 ==============================================================================================================================================================*/
-
 #include<stdio.h>
 long long int calval(char * c) // it returns the integer value of the string entered
 {
 	long long  int val1=0,i;
-	for(i=0;c[i]!='\0';i++)
+		for(i=0;c[i]!='\0';i++)
 	{
 		val1 = val1*10+(c[i]-48);
-		
 	}
 	return val1;
 }
@@ -34,88 +32,44 @@ int lenval(char *c) // it returns the length of the string which is used in mani
 	}
 	return i;
 }
-char * nextpal(char *c,int l,long long int val1)
+char  mini(char c,char d) // find the min of mean position value for length = even
 {
-	int diff,flag=0;
-	long long int val2;
-	val2 = calval(c);
-	if(l%2==0) // if the number entered has even no of digits
+	char min;
+	min = c;
+	if(d<c)
 	{
-	//4-1,4-2, 6-2,6-3, 8-3,8-4
-	//length of number -> meanpos-1,meanpos to modify
-	diff = l/2; // mean positions to increment
-	while(val2<=val1)
-	{
-		c[diff]   = c[diff]+1;
-		c[diff-1] = c[diff]; // even length number will have two  mean digits
-		if(c[diff]>9+48) // if mean pos number becomes greater than 9 set the value as  digit to lowest length+1 number
-		{
-			flag = 1;
-			int i;
-			c[0] =  c[0] + 1;
-			for(i=1;c[i]!='\0';i++)
-			{
-				c[i] = '0';
-			}
-			val1 = calval(c);
-			c[l-1] = c[0];
-			c = nextpal(c,l,val1);
-			
-		}
-		if(flag==1)
-		{
-			break;
-		}
-		val2 = calval(c);
+		min = d;
 	}
-	return c;
-	}
-	else
-	{
-	//3->1 5-2, 7-3,9-4,11->5  
-	 //length of string ->position to modify
-	  	diff = (int)l/2;
-	  	while(val2<=val1)
-	  	{
-	  		c[diff] = c[diff]+1;
-	  		if(c[diff]>9+48)
-			{
-				flag = 1;
-				int i;
-				c[0] =  c[0] + 1;
-				for(i=1;c[i]!='\0';i++)
-				{
-					c[i] = '0';
-				}
-				val1 = calval(c);
-				c[l-1] = c[0];
-				c = nextpal(c,l,val1);
-			}
-			if(flag==1)
-			{
-				break;
-			}
-			val2 = calval(c);
-		}	
-	  return c;
+	return min;
+	
 }
-}
-int main()
+char * rotate(char *c,int diff,int l) // function to find mirror image
 {
-	int l,i;
-	long long int v;
-	char val[20],*d;
-	scanf("%s",val);
+		int i,j;
+		if(l%2!=0)
+		{
+			for(i=1,j=diff+1;c[j]!='\0';j++,i++)
+			{
+				c[j] = c[diff-i];
+			}
+		}
+		else
+		{ 
+			for(i=0,j=diff+1;i<diff-1;j++,i++)
+			{
+				c[j] = c[diff-2-i];
+			}
+		}
+		return c;
+}
+char * nextpal(char * val) 
+{
+	int l,diff,i,j,p;
+	long long int val1,val2;
+	val1 = calval(val);
 	l = lenval(val);
-	v = calval(val);
-	if(v<9)
+	if(val1==9||val1==99||val1==999||val1==9999||val1==99999||val1==999999||val1==9999999||val1==99999999)// for special cases the number is set to the smallest length+1 number
 	{
-		printf("next palindrome = %d\n",v+1);
-	}
-	else
-	{
-		if(v==9||v==99||v==999||v==9999||v==99999||v==999999||v==9999999||v==99999999)// for special cases the number is set to the smallest length+1 number
-		{
 			val[0] = '1';
 			for(i=1;val[i]!='\0';i++)
 			{
@@ -123,12 +77,79 @@ int main()
 			}
 			val[l] = '0';
 			val[l+1] = '\0';
-			l = l+1;
+			l= l+2;
+			char * e = nextpal(val);
+			return e;
+			
+	}
+	char *d;
+	diff = l/2; // for finding mean position
+	if(l%2==0)
+	{
+		
+		if(val[diff]!=val[diff-1])
+		{
+		char e = mini(val[diff],val[diff-1]);
+		val[diff] = e;
+		val[diff-1] = e;
 		}
-	
-		val[l-1] =  val[0];
-		d = nextpal(val,l,v);
-		printf("next pal = %s\n",d);
+		d = rotate(val,diff,l);
+		val2 = calval(d);
+		while(val2<=val1)
+		{
+			
+			val[diff] = val[diff]+1;
+			val[diff-1] = val[diff];
+			if(val[diff]>9+48) 
+			{
+			int i;
+			val[0] = val[0] + 1;
+			for(i=1;val[i]!='\0';i++)
+			{
+				val[i] = '0';
+			}
+			d = nextpal(val);
+			break;
+			}
+			d = rotate(val,diff,l);
+			val2 = calval(d);
+		}
+	}
+	else
+	{
+		d = rotate(val,diff,l);
+		val2 = calval(d);
+		while(val2<=val1)
+		{
+			val[diff] = val[diff] + 1;
+			if(val[diff]>9+48) 
+			{
+			val[0] = val[0] + 1;
+			for(i=1;i<l;i++)
+			{
+				val[i] = '0';
+			}
+			d = nextpal(val);
+			break;
+			}
+			d=rotate(val,diff,l);
+			val2 = calval(d);
+		}
+	}
+	return d;
+}
+int main()
+{
+	char val[14];
+	scanf("%s",&val);
+	if(lenval(val)==1&&val[0]<48+9)
+	{
+		val[0] = val[0] + 1;
+		printf("val = %s\n",val);
+	}
+	else
+	{
+		printf("val = %s\n",nextpal(val));
 	}
 	return 0;
 }
